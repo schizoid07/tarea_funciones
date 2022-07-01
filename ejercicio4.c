e108d579e3ef3be9dfeda09a145fa0ded0034b3f
Miguel Nunez <miguelhero700@gmail.com>
2022-07-01 18:13:34 -0400

    enunciado?ejercicio4

--- ejercicio4.c
+++ ejercicio4.c
@@ -1,7 +1,13 @@
+/*Ejercicio 4. Escribir una función que calcule y presente las raíces reales de la
+ecuación de segundo grado
+ax2+bx+c=O
+Suponer que a, b y c son argumentos en coma flotante con valores dados y que x1
+y que x2 son variables en coma flotante. Suponer también que b'>4*a*c, de forma
+que las raíces calculadas son siempre reales*/
 #include <stdio.h>
 #include <math.h>
 
-float raiz_real1(float a, float b, float c);
+void raiz_real1(float a, float b, float c);
 
 
 int main () {
@@ -13,7 +19,7 @@ raiz_real1(a,b,c);
 return 0;
 }
 
-float raiz_real1(float a, float b, float c) {
+void raiz_real1(float a, float b, float c) {
   float x1=((-b)+sqrt((b*b)-(4*a*c)))/(2*a);
   float x2=((-b)-sqrt((b*b)-(4*a*c)))/(2*a);
   printf("\nel valor de x1 es: %2f",x1);
